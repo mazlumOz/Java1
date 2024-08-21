a491865fdcafb1fd75003deb72640573cf018012
mazlumoz <mazlum_ozdemir-04@hotmail.com>
2024-08-21 20:26:28 +0300

    Sepet test eklendi

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -6,9 +6,12 @@ public class Merhaba {
 
         System.out.println("Merhaba 2");
 
+        System.out.println("Task 1 kodları");
+
 
 
     }
 }
 //Kırmızı renk: Bu dosya henüz takip edilmiyor
 // Yeliş renk: Bu dosya Takip ediliyor, yani değişiklikler kaydedilecek
+// comit : ozaman kadar yaptığın değişikliklere bir isim vermek demektir.
