e54f1998772a2ed37bf7b7355e09c2ea31a90018
mazlumoz <mazlum_ozdemir-04@hotmail.com>
2024-08-21 20:33:42 +0300

    wish List Testi eklendi

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -4,11 +4,15 @@ public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
 
-        System.out.println("Merhaba 2");
-
         System.out.println("Task 1 kodları");
 
         System.out.println("Task 2 kodları");
+
+        System.out.println("Task 3 kodları");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Task4 yapıldı");
+        }
 
     }
 }
