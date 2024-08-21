08f4c4ca4d01c98c98dee68e3eb73d910e8d9726
mazlumoz <mazlum_ozdemir-04@hotmail.com>
2024-08-21 20:21:32 +0300

    Login Testi eklendi (Sana gelen işin adı, Jiradan gelen)

---
+++ src/Gun01/Merhaba.java
@@ -0,0 +1,14 @@
+package Gun01;
+
+public class Merhaba {
+    public static void main(String[] args) {
+        System.out.println("Merhaba 1");
+
+        System.out.println("Merhaba 2");
+
+
+
+    }
+}
+//Kırmızı renk: Bu dosya henüz takip edilmiyor
+// Yeliş renk: Bu dosya Takip ediliyor, yani değişiklikler kaydedilecek
